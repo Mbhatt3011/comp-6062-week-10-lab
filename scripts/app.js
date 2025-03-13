ab19154e8b2b31e9e7507f8619145494c0d9e7ab
Mbhatt3011 <bhattmaharshi09@gmail.com>
2025-03-13 19:18:28 -0400

    Week 10

---
+++ scripts/app.js
@@ -0,0 +1,32 @@
+
+console.log('JS loaded!');
+const app = Vue.createApp({
+    data() {
+        return {
+            mainHeading: 'Week 10 Lab',
+            firstName:'',
+            lastName:'',
+            quantity: 0,
+            pricePerItem: 10
+        };
+    },
+    computed: {
+        fullName() {
+          return this.firstName + ' '+ this.lastName
+        },
+        totalPrice() {
+          return this.quantity * this.pricePerItem;
+        }
+      },
+
+      methods: {
+        clearFields() {
+          this.firstName = '';
+          this.lastName = '';
+          this.quantity = 0;
+        }
+      }
+    });
+
+    app.mount('#app');
+
